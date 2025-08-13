876d34b8ae12ecce6f8832a7643be818399ac2f7
Jonathan Yunawan <jyunawan@gmail.com>
2025-08-13 12:40:48 -0700

    Add API views for Team and Match endpoints

--- backend/vlr_data/views.py
+++ backend/vlr_data/views.py
@@ -1,5 +1,36 @@
+from datetime import timedelta
+from django.utils import timezone
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 
+from .models import Team, Match
+from .serializers import MatchSerializer, TeamSerializer
 
+
+class TeamListView(ListAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class TeamDetailView(RetrieveAPIView):
+    queryset = Match.objects.all()
+    serializer_class = TeamSerializer
+    lookup_field = "vlr_id"
+
+
+class MatchListView(ListAPIView):
+    queryset = Match.objects.all().order_by("-date_played")
+    serializer_class = MatchSerializer
+
+
+class MatchDetailView(RetrieveAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+    lookup_field = "vlr_id"
+
+
+class UpcomingMatchView(ListAPIView):
+    queryset = Match.objects.filter(is_finished=False).order_by("-date_played")
+    serializer_class = MatchSerializer
