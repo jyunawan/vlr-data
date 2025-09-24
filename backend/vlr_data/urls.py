63dc360a6bec39ac94b4b19666d588bceeadfd23
Jonathan Yunawan <jyunawan@gmail.com>
2025-09-24 00:41:34 -0700

    Added a new PlayerDetailView to look up player data

--- backend/vlr_data/urls.py
+++ backend/vlr_data/urls.py
@@ -5,12 +5,14 @@ from .views import (
     TeamDetailView,
     TeamListView,
     UpcomingMatchView,
+    PlayerDetailView
 )
 
 urlpatterns = [
-    path("teams/", TeamListView.as_view(), name="team_list"),
-    path("team/<str:vlr_id>/", TeamDetailView.as_view(), name="team_detail"),
-    path("matches/", MatchListView.as_view(), name="match_list"),
-    path("match/<str:vlr_id>/", MatchDetailView.as_view(), name="match_detail"),
-    path("upcoming_matches/", UpcomingMatchView.as_view(), name="upcoming_matches"),
+    path("teams", TeamListView.as_view(), name="team_list"),
+    path("team/<str:vlr_id>", TeamDetailView.as_view(), name="team_detail"),
+    path("matches", MatchListView.as_view(), name="match_list"),
+    path("match/<str:vlr_id>", MatchDetailView.as_view(), name="match_detail"),
+    path("upcoming_matches", UpcomingMatchView.as_view(), name="upcoming_matches"),
+    path("player/<str:vlr_id>", PlayerDetailView.as_view(), name="player_detail"),
 ]
--- backend/vlr_data/views.py
+++ backend/vlr_data/views.py
@@ -5,32 +5,49 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 
-from .models import Team, Match
-from .serializers import MatchSerializer, TeamSerializer
+from .models import Player, Team, Match
+from .serializers import MatchSerializer, PlayerSerializer, TeamSerializer
 
 
 class TeamListView(ListAPIView):
+    permission_classes = (permissions.AllowAny, )
+    
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
 
 class TeamDetailView(RetrieveAPIView):
-    queryset = Match.objects.all()
+    permission_classes = (permissions.AllowAny, )
+        
+    queryset = Team.objects.all()
     serializer_class = TeamSerializer
     lookup_field = "vlr_id"
 
 
 class MatchListView(ListAPIView):
+    permission_classes = (permissions.AllowAny, )
+        
     queryset = Match.objects.all().order_by("-date_played")
     serializer_class = MatchSerializer
 
 
 class MatchDetailView(RetrieveAPIView):
+    permission_classes = (permissions.AllowAny, )
+        
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
     lookup_field = "vlr_id"
 
 
 class UpcomingMatchView(ListAPIView):
-    queryset = Match.objects.filter(is_finished=False).order_by("-date_played")
+    permission_classes = (permissions.AllowAny, )
+    
+    queryset = Match.objects.filter(is_finished=False).order_by("date_played")
     serializer_class = MatchSerializer
+    
+class PlayerDetailView(RetrieveAPIView):
+    permission_classes = (permissions.AllowAny, )
+        
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    lookup_field = "vlr_id"
