a1db850ed13d4d936af5de22044054b65fe77789
Jonathan Yunawan <jyunawan@gmail.com>
2025-09-25 17:18:31 -0700

    Added some details to the MatchSerializer to reduce the number of API calls

--- backend/vlr_data/serializers.py
+++ backend/vlr_data/serializers.py
@@ -10,16 +10,24 @@ class TeamSerializer(ModelSerializer):
 
 
 class MatchSerializer(ModelSerializer):
-    team1 = CharField(source="team1.vlr_id", read_only=True)
-    team2= CharField(source="team2.vlr_id", read_only=True)
+    team1_id = CharField(source="team1.vlr_id", read_only=True)
+    team2_id = CharField(source="team2.vlr_id", read_only=True)
+    team1_logo = CharField(source="team1.team_logo_url", read_only=True)
+    team2_logo = CharField(source="team2.team_logo_url", read_only=True)
+    team1 = CharField(source="team1.name", read_only=True)
+    team2 = CharField(source="team2.name", read_only=True)
     event = CharField(source="event.vlr_url", read_only=True)
     
     class Meta:
         model = Match
         fields = [
             "event",
+            "team1_id",
+            "team2_id",
             "team1",
             "team2",
+            "team1_logo",
+            "team2_logo",
             "date_played",
             "vlr_id",
             "is_finished",
